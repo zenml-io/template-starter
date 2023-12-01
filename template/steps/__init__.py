2dd1ac4b0d705adcd6b90a3521ee2d79b889d8dc
Hamza Tahir <htahir111@gmail.com>
2023-12-01 09:38:05 +0100

    Template

--- template/steps/__init__.py
+++ template/steps/__init__.py
@@ -1,17 +1,23 @@
 # {% include 'template/license_header' %}
 
-from data_loader import (
+from .data_loader import (
     data_loader,
 )
-from data_preprocessor import (
+from .data_preprocessor import (
     data_preprocessor,
 )
-from data_splitter import (
+from .data_splitter import (
     data_splitter,
 )
-from model_evaluator import (
+from .model_evaluator import (
     model_evaluator,
 )
-from model_trainer import (
+from .model_trainer import (
     model_trainer,
+)
+from .inference_preprocessor import (
+    inference_preprocessor,
+)
+from .inference_predict import (
+    inference_predict,
 )
